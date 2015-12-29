5a35a1c1b3a062a364d754b93dfe7a74f916b35d
zhaolu <zhaolu@playcrab.com>
2015-12-29 12:00:13 +0800

    禁用crsf验证

--- config/web.php
+++ config/web.php
@@ -11,7 +11,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'playcrab',
-            'enableCookieValidation' => false,
+            'enableCsrfValidation' => false,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
