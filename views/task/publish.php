9fcd14e8f70dee6b472380b5cd0c8bb86c95ed9d
playcrab <lnlmaxtvxq@163.com>
2015-12-29 16:07:17 +0800

    发布任务界面

--- views/task/publish.php
+++ views/task/publish.php
@@ -2,99 +2,322 @@
 
 /* @var $this yii\web\View */
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 $this->title = 'My Yii Application';
 ?>
- <?= Html::cssFile('@web/static/plugins/select2-3.5.2/select2.css'); ?>
+<?= Html::cssFile('@web/static/plugins/select2-3.5.2/select2.css'); ?>
+<?= Html::cssFile('@web/static/plugins/awesome-bootstrap-checkbox/awesome-bootstrap-checkbox.css'); ?>
 
-'',
 <div class="content animate-panel">
-<div class="row">
+<form method="post" class="form-horizontal" action="/task/republish">
+<input name="_csrf" type="hidden" id="_csrf" value="<?= Yii::$app->request->csrfToken ?>">
+<div class="content">
+    <div class="row">
     <div class="col-lg-6">
-        <div class="hpanel">
+         <div class="hpanel">
             <div class="panel-heading hbuilt">
                 <div class="panel-tools">
-                    <a class="showhide"><i class="fa fa-chevron-up"></i></a>
-                    <a class="closebox"><i class="fa fa-times"></i></a>
+<!--                     <a class="showhide"><i class="fa fa-chevron-up"></i></a> -->
+<!--                     <a class="closebox"><i class="fa fa-times"></i></a> -->
                 </div>
                 选择发布位置和任务目标
             </div>
-            <div class="panel-body no-padding">
                 <div class="panel-body">
-                   <form method="get" class="form-horizontal">
-                        <div class="form-group"><label class="col-sm-2 control-label">发布位置</label>
-                            <div class="col-sm-10">
-                                <select class="js-source-states"  style="width: 100%">
-                            <option value="Blue">Blue</option>
-                            <option value="Red">Red</option>
-                            <option value="Green">Green</option>
-                            <option value="Maroon">Maroon</option>
-                    </select>
+                        <div class="form-group"><label class="col-sm-3 control-label">发布位置</label>
+                            <div class="col-sm-9">
+                                <select class="js-source-states" name="deployment_id" style="width: 100%">
+                                    <option value="1">test1.war.playcrab.com</option>
+                                    <option value="2">test2.war.playcrab.com</option>
+                                    <option value="3">test3.war.playcrab.com</option>
+                                    <option value="4">test4.war.playcrab.com</option>
+                                </select>
                             </div>
                         </div>
-                        <div class="form-group"><label class="col-sm-2 control-label">Help text</label>
+                        <div class="hr-line-dashed"></div>
+                        <!--  
+                        <div class="form-group"><label class="col-sm-3 control-label">walle worker</label>
         
-                            <div class="col-sm-10"><input type="text" class="form-control"> <span class="help-block m-b-none">A block of help text that breaks onto a new line and may extend beyond one line.</span>
+                            <div class="col-sm-9">
+                                 <select class="js-source-states" name="work_id" style="width: 100%">
+                                    <option value="1">test1.war.playcrab.com</option>
+                                    <option value="2">test2.war.playcrab.com</option>
+                                    <option value="3">test3.war.playcrab.com</option>
+                                    <option value="4">test4.war.playcrab.com</option>
+                                </select>
                             </div>
                         </div>
-                        <div class="form-group"><label class="col-sm-2 control-label">Password</label>
-        
-                            <div class="col-sm-10"><input type="password" class="form-control" name="password"></div>
-                        </div>
-                        <div class="form-group"><label class="col-sm-2 control-label">Placeholder</label>
-        
-                            <div class="col-sm-10"><input type="text" placeholder="placeholder" class="form-control"></div>
-                        </div>
-                        <div class="form-group"><label class="col-lg-2 control-label">Disabled</label>
-        
-                            <div class="col-lg-10"><input type="text" disabled="" placeholder="Disabled input here..." class="form-control"></div>
-                        </div>
-                        <div class="form-group"><label class="col-lg-2 control-label">Static control</label>
-        
-                            <div class="col-lg-10"><p class="form-control-static">email@example.com</p></div>
-                        </div>
-                        <div class="form-group">
-                            <div class="col-sm-8 col-sm-offset-2">
-                                <button class="btn btn-default" type="submit">Cancel</button>
-                                <button class="btn btn-primary" type="submit">Save changes</button>
+                        -->
+                        <div class="form-group"><label class="col-sm-3 control-label">服务端更新包</label>
+                            <div class="col-md-2 col-md-offset-7">
+                                <div class="checkbox checkbox-success">
+                                    <input id="upload_server_update_package" type="checkbox" name="target_tasks" checked value="upload_server_update_package">
+                                    <label for="upload_server_update_package">
+                                    </label>
+                                </div>
                             </div>
                         </div>
-                    </form>
+                        <div class="hr-line-dashed"></div>
+                        <div class="form-group"><label class="col-sm-3 control-label">客户端更新包</label>
+        
+                            <div class="col-md-2 col-md-offset-7">
+                                <div class="checkbox checkbox-warning">
+                                    <input id="create_client_update_package" type="checkbox" name="target_tasks" checked onclick="javascript:checkUpdateClient()" value="create_client_update_package">
+                                    <label for="create_client_update_package">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="hr-line-dashed"></div>
+                        <div class="form-group"><label class="col-md-3 control-label">客户端安装包</label>
+        
+                             <div class="col-md-2 col-md-offset-7">
+                                <div class="checkbox checkbox-danger">
+                                    <input id="create_client_package" type="checkbox" name="target_tasks" checked onclick="javascript:checkClient()" value="create_client_package">
+                                    <label for="create_client_package">
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                       
+                 
                   </div>
-            </div>
             <div class="panel-footer">
-                This is standard panel footer
             </div>
         </div>
     </div>
     <div class="col-lg-6">
-        <div class="hpanel">
+        <div id="div2" class="hpanel"  >
             <div class="panel-heading hbuilt">
                 <div class="panel-tools">
-                    <a class="showhide"><i class="fa fa-chevron-up"></i></a>
-                    <a class="closebox"><i class="fa fa-times"></i></a>
                 </div>
-                选择客户端更新包
+                 <div class="row">
+
+                    <label class="col-md-3 control-label">选择客户端更新包 </label>
+
+                     <div class="col-md-4 col-md-offset-2">
+
+                    	<div class="checkbox checkbox-primary">
+
+                        	<input id="chk_all" type="checkbox" onclick="javascript:chkall()" checked/><label for="chk_all">选中最近30天版本</label>
+
+                       </div>
+
+                	  </div>
+                   </div>  
             </div>
-            <div class="alert alert-success">
-                <i class="fa fa-bolt"></i> Adding action was successful
-            </div>
-            <div class="panel-body">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan. Aliquam ornare lacus adipiscing, posuere lectus et, fringilla augue.Lorem
-                    ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt est vitae ultrices accumsan.
-                </p>
+            <div class="panel-body"  style=" height:250px; overflow-y:scroll;overflow-x:hidden;">
+                <div class="table-responsive" id="version_div" >
+                <table class="table table-condensed table-striped" cellpadding="1" cellspacing="1"  style="table-layout:fixed;" >
+                    <thead>
+                    <tr>
+                        <th>版本号</th>
+                        <th>发布时间</th>
+                        <th>是否选择</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="1" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="2" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="3" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="4" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="5" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="6" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="7" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="8" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="9" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="10" checked></td>
+                    </tr>
+                           <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="11" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="12" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="13" checked></td>
+                    </tr>
+                    <tr>
+                        <td>112</td>
+                        <td>2015-12-28 16:09:22</td>
+                        <td><input id="chk_all_1" type="checkbox" name="package_update_config" value="14" checked></td>
+                    </tr>
+                    </tbody>
+                </table>
+              </div>
             </div>
             <div class="panel-footer">
-                This is standard panel footer
             </div>
         </div>
     </div>
 </div>
+<div class="row">
+    <div class="col-lg-6">
+         <div id="div3" class="hpanel">
+            <div class="panel-heading hbuilt">
+                <div class="panel-tools">
+                </div>
+                发布相关的动态可调整参数
+            </div>
+                <div class="panel-body">
+                        <div class="form-group"><label class="col-sm-4 control-label">打包机</label>
+        
+                            <div class="col-sm-8">
+                                 <input  type="text" name="work_id" value="test1.war.playcrab.com" style="width: 100%" readonly>
+                            </div>
+                        </div>
+                        <div class="form-group"><label class="col-sm-4 control-label">日志级别</label>
+                            <div class="col-sm-8">
+                                <select class="js-source-states" name="log_level" style="width: 100%">
+                                    <option value="DEBUG">DEBUG</option>
+                                    <option value="INFO">INFO</option>
+                                    <option value="WARNING">WARNING</option>
+                                    <option value="ERROR">ERROR</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group"><label class="col-sm-4 control-label">并发执行的task数量</label>
+                            <div class="col-md-8">
+                                    <input id="concurrent_task_count" type="text" name="concurrent_task_count" value="1">
+                            </div>
+                        </div>
+                       
+                 </div>
+            <div class="panel-footer">
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-6">
+        <div id="div4" class="hpanel"  >
+            <div class="panel-heading hbuilt">
+                <div class="panel-tools">
+                </div>
+                选择客户端安装包
+            </div>
+            <div class="panel-body" >
+                <div class="table-responsive" id="version_div" >
+                <table class="table table-condensed table-striped" cellpadding="1" cellspacing="1"  style="table-layout:fixed;" >
+                    <thead>
+                    <tr>
+                        <th>安装包</th>
+                        <th>是否选择</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>appstore_debug</td>
+                        <td><input id="package_1" type="checkbox" name="package_config" value="3" checked></td>
+                    </tr>
+                    <tr>
+                        <td>appstore_release</td>
+                        <td><input id="package_2" type="checkbox" name="package_config" value="1" checked></td>
+                    </tr>
+                     <tr>
+                        <td>appstore_debug</td>
+                        <td><input id="package_3" type="checkbox" name="package_config" value="2" checked></td>
+                    </tr>
+                    <tr>
+                        <td>appstore_release</td>
+                        <td><input id="package_4" type="checkbox" name="package_config" value="4" checked></td>
+                    </tr>
+                    </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="panel-footer">
+            </div>
+        </div>
+    </div>
+   </div>
+   <button type="submit" class="btn btn-success btn-block">下一步</button>
+  </form>
+  </div>
 </div>
  <?= Html::jsFile('@web/static/plugins/select2-3.5.2/select2.min.js'); ?>
 <script type="text/javascript">
  $(function(){
 	 $(".js-source-states").select2();  
  });
+ function chkall()
+ {
+     var val = $('#chk_all').prop('checked');
+     if(val == true)
+     {
+         $('#version_div :checkbox').prop('checked', true);
+     }
+     if(val == false)
+     {
+         $('#version_div :checkbox').prop('checked', false);
+     }
+ }
+
+ function checkUpdateClient()
+ {
+     var val = $('#create_client_update_package').prop('checked');
+     if(val == true)
+     {
+     	 $("#div2").show();
+     }
+     if(val == false)
+     {
+    	 $("#div2").hide();
+     }
+ }
+ function checkClient()
+ {
+     var val = $('#create_client_package').prop('checked');
+     if(val == true)
+     {
+    	 $("#div4").show();
+     }
+     if(val == false)
+     {
+    	 $("#div4").hide();
+     }
+ }
  </script>
