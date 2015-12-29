54fdab4e1f4fff0a202fd6223aedd349e4ba9954
zhaolu <zhaolu@playcrab.com>
2015-12-29 11:45:59 +0800

    禁用cookie验证

--- config/web.php
+++ config/web.php
@@ -11,6 +11,7 @@ $config = [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'playcrab',
+            'enableCookieValidation' => false,
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
