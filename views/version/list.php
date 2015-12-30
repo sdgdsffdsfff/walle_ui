e14b9a62cca48f1b325c66e710d2895273a83657
zhaoshuang <zhaoshuang@playcrab.com>
2015-12-30 12:51:03 +0800

    ad

--- views/version/list.php
+++ views/version/list.php
@@ -7,6 +7,8 @@ use \yii\widgets\LinkPager;
 <?= Html::cssFile('@web/static/plugins/select2-bootstrap/select2-bootstrap.css'); ?>
 <?= Html::cssFile('@web/static/plugins/summernote/dist/summernote.css'); ?>
 <?= Html::cssFile('@web/static/plugins/summernote/dist/summernote-bs3.css'); ?>
+<?= Html::cssFile('@web/static/plugins/bootstrap-datepicker-master/dist/css/bootstrap-datetimepicker.min.css'); ?>
+
 <div class="normalheader transition small-header">
     <div class="hpanel">
         <div class="panel-body">
@@ -37,20 +39,28 @@ use \yii\widgets\LinkPager;
   </div>
   <div class="form-group">
     <label for="exampleInputEmail2">升级序列</label>
-    <select class="js-source-states" name="upagrade_path_id" >
-                                    <optgroup label="请选择发行区域-平台">
-                                        <option value="AK">大陆发行-appstore</option>
-                                        <option value="HI">海外发行-appstore</option>
+    <select class="js-source-states" name="upagrade_path_id" style="width:200px">
+                                    <optgroup label="请选择升级序列">
+                                         <option value="">请选择升级序列</option>
+                                        <?php 
+                                        // foreach ($platform as $v) {
+                                        //     echo " <option value='".$v['id']."'>".$v['name']."</option>";
+                                        // }
+                                        ?>
                                     </optgroup>
                                 </select>
   </div>
 
      <div class="form-group">
     <label for="exampleInputEmail2">平台</label>
-    <select class="js-source-states" name="platform_id" >
+    <select class="js-source-states" name="platform_id" style="width:200px" >
                                     <optgroup label="请选择发行区域-平台">
-                                        <option value="AK">大陆发行-appstore</option>
-                                        <option value="HI">海外发行-appstore</option>
+                                        <option value="">请选择发行区域-平台</option>
+                                        <?php 
+                                        // foreach ($platform as $v) {
+                                        //     echo " <option value='".$v['id']."'>".$v['region']['description']."-".$v['name']."</option>";
+                                        // }
+                                        ?>
                                     </optgroup>
                                 </select>
   </div>
@@ -71,20 +81,33 @@ use \yii\widgets\LinkPager;
                             <th>创建人</th>
                             <th>change log</th>
                             <th>业务模块版本</th>
+                            <th>上线时间</th>
                             <th>操作</th>
                             
                         </tr>
                         </thead>
                         <tbody>
                              <tr>
-                        <td><a style="text-decoration:underline" href="#">122</a></td>
-                        <td>96</td>
-                        <td>deploy1.saiya.playcrab-inc.com</td>
-                        <td>2015-12-28 22:22:10</td>
-                        <td>2015-12-28 22:42:10</td>
+                        <td><a style="text-decoration:underline" href="/version/version-detail">122</a></td>
+                        <td>2015-12-28 22:42:10<</td>
+                        <td>大陆发行-appstore</td>
+                        <td>23sdsfappstore</td>
+                        <td>zhaoshuang</td>
                         <td>成功</td>
-                        <td>liuhaiyang</td>
-                        <td>生成安装包</td>
+                        <td>frontend:master.1512.34.45</br>
+                            backend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                        </td>
+                        <td> 
+                             <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal6">
+                       设置时间
+                    </button>
+                        </td>
+                        <td><a href="/version/add-version" class="btn btn-info btn-sm active" >发布版本</a>
+<a href="/task/list" class="btn btn-success btn-sm active" >查看相关任务</a></td>
                    
                     </tr>
                                 <tr>
@@ -94,8 +117,16 @@ use \yii\widgets\LinkPager;
                         <td>2015-12-28 22:22:10</td>
                         <td>2015-12-28 22:42:10</td>
                         <td>成功</td>
-                        <td>liuhaiyang</td>
-                        <td>生成安装包</td>
+                         <td>frontend:master.1512.34.45</br>
+                            backend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                        </td>
+                        <td>2015-12-28 22:42:10</td>
+                        <td><a href="/version/add-version" class="btn btn-info btn-sm active" >发布版本</a>
+<a href="/task/list" class="btn btn-success btn-sm active" >查看相关任务</a></td>
                    
                     </tr>
                                 <tr>
@@ -105,8 +136,16 @@ use \yii\widgets\LinkPager;
                         <td>2015-12-28 22:22:10</td>
                         <td>2015-12-28 22:42:10</td>
                         <td>成功</td>
-                        <td>liuhaiyang</td>
-                        <td>生成安装包</td>
+                          <td>frontend:master.1512.34.45</br>
+                            backend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                        </td>
+                        <td>2015-12-28 22:42:10</td>
+                        <td><a href="/version/add-version" class="btn btn-info btn-sm active" >发布版本</a>
+<a href="/task/list" class="btn btn-success btn-sm active" >查看相关任务</a></td>
                    
                     </tr>
                                 <tr>
@@ -116,21 +155,19 @@ use \yii\widgets\LinkPager;
                         <td>2015-12-28 22:22:10</td>
                         <td>2015-12-28 22:42:10</td>
                         <td>成功</td>
-                        <td>liuhaiyang</td>
-                        <td>生成安装包</td>
+                          <td>frontend:master.1512.34.45</br>
+                            backend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                            frontend:master.1512.34.45</br>
+                        </td>
+                        <td> <button type="button" class="btn btn-primary btn-sm">设置时间</button></td>
+                        <td><a href="/version/add-version" class="btn btn-info btn-sm active" >发布版本</a>
+<a href="/task/list" class="btn btn-success btn-sm active" >查看相关任务</a></td>
                    
                     </tr>
-                                <tr>
-                        <td><a style="text-decoration:underline" href="#">122</a></td>
-                        <td>96</td>
-                        <td>deploy1.saiya.playcrab-inc.com</td>
-                        <td>2015-12-28 22:22:10</td>
-                        <td>2015-12-28 22:42:10</td>
-                        <td>成功</td>
-                        <td>liuhaiyang</td>
-                        <td>生成安装包</td>
-                   
-                    </tr>
+                             
                            <?php 
                             // foreach ($models as $k) {
                             //     echo "<tr>";
@@ -171,7 +208,29 @@ use \yii\widgets\LinkPager;
 
     </div>
     </div>
-
+          <div class="modal fade" id="myModal6" tabindex="-1" role="dialog"  aria-hidden="true">
+                        <div class="modal-dialog modal-sm">
+                            <div class="modal-content">
+                                <div class="color-line"></div>
+                                <div class="modal-header">
+                                    <h4 class="modal-title">Modal title</h4>
+                                    <small class="font-bold">设置时间</small>
+                                </div>
+                                <div class="modal-body">
+                                    
+                            <div class="input-group date">
+                            <input type="text" class="form-control" size="16" value="2016-01-1 21:05" id="datetimepicker" data-date-format="yyyy-mm-dd hh:ii">
+                           
+                        </div>
+                  
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">关闭</button>
+                                    <button type="button" class="btn btn-primary">保存</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
     <!-- Footer-->
 
@@ -179,10 +238,24 @@ use \yii\widgets\LinkPager;
 
 <?= Html::jsFile('@web/static/plugins/select2-3.5.2/select2.min.js'); ?>
 <?= Html::jsFile('@web/static/plugins/summernote/dist/summernote.min.js'); ?>
+<?= Html::jsFile('@web/static/plugins/bootstrap-datepicker-master/dist/js/bootstrap-datetimepicker.min.js'); ?>
+<?= Html::jsFile('@web/static/plugins/bootstrap-datepicker-master/dist/js/bootstrap-datetimepicker.zh-CN.js'); ?>
+
 
 <script>
 
     $(function () {
+          $('#datetimepicker').datetimepicker({
+        //language:  'fr',
+        weekStart: 1,
+        todayBtn:  1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        forceParse: 0,
+        showMeridian: 1,
+         format: 'yyyy-mm-dd hh:ii'
+    });
  $(".js-source-states").select2();
     });
 
