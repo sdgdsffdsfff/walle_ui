2a8e3518623d3a519a871771acb559a4f98921a8
zhaoshuang <zhaoshuang@playcrab.com>
2016-01-07 16:24:18 +0800

    index

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -8,23 +8,54 @@ namespace app\controllers;
 use yii;
 use yii\web\Controller;
 use clients\ucenter\services\Game as Game;
-
+use clients\ucenter\services\MainCategory as MainCategory;
 class IndexController extends Controller
 {
 	public $layout = "vms_index";
     public function actionIndex()
     {	
-        $gameInfo = Game::getAll();   
+        
+         $userRoles = Yii::$app->getUser()->getIdentity()->getUserRoles();
+            // var_dump($userRoles);die; 
+            foreach ($userRoles as $key => $role)
+        {
+            if($role['game_id'] == 0)
+            {
+                $gameInfo = Game::getAll();              
+                break;
+            }
+            else
+            {
+                $info=Game::getGameById($role['game_id']);
+                $gameInfo[$key] = array(
+                    'id' => $role['game_id'],
+                    'name' => $info['name'],  //获取游戏名称
+                    'alias'=>$info['alias']
+                );
+            }
+        }
+
         return $this->render('index',['gameInfo'=>$gameInfo]);
     }
 
     public function actionSeldb()
     {
+ 
     	$alias = yii::$app->getRequest()->get('alias');
-        //默认会开启session
-        yii::$app->session->set('game_alias', $alias);
-
-    	header('Location:/version/add-version'); 
-    	exit;
+        $id = yii::$app->getRequest()->get('id');
+        $userRoles = Yii::$app->getUser()->getIdentity()->getUserRoles();
+        foreach ($userRoles as $key => $role)
+        {
+            if($role['game_id'] == 0||$role['game_id']==$id)
+            {
+                //默认会开启session
+                yii::$app->session->set('game_alias', $alias);
+                header('Location:/version/add-version'); 
+                exit;
+            }
+           
+        }
+        echo "对不起，您没有权限";exit;
+      
     }
 }
