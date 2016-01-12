4a1b5b62297e97082225559c88a02d90a7a9b929
zhaoshuang <zhaoshuang@playcrab.com>
2016-01-12 13:34:15 +0800

    update

--- config/dbConfig.php
+++ config/dbConfig.php
@@ -23,6 +23,20 @@ return [
             'password' => '123456',
             'charset' => 'utf8',
         ],
+          'icx' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=walle_dev',
+            'username' => 'walle',
+            'password' => 'wW#3HdCDp&9r',
+            'charset' => 'utf8',
+        ],
+          'war' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;dbname=walle_dev_war',
+            'username' => 'walle',
+            'password' => 'wW#3HdCDp&9r',
+            'charset' => 'utf8',
+        ],
     ]
 ];
 
