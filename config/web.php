f8ffc54cbc45607da00e6666ac33bf09647452d4
zhaolu <zhaolu@playcrab.com>
2016-01-21 11:59:57 +0800

    up

--- config/web.php
+++ config/web.php
@@ -9,15 +9,7 @@ $params = array_merge(
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    //'bootstrap' => ['log'],
-    'bootstrap' => ['debug'],
-    //yii debug tools
-    'modules' => [
-        'debug' => [
-            'class' => 'yii\debug\Module',
-            'allowedIPs' => ['172.16.148.244', '127.0.0.1', '::1']
-        ]
-    ],
+    'bootstrap' => ['log'],
     'defaultRoute' => 'index', //默认控制器
     'components' => [
         'request' => [
@@ -72,6 +64,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'debug';
     $config['modules']['debug'] = [
         'class' => 'yii\debug\Module',
+        'allowedIPs' => ['172.16.148.244', '127.0.0.1', '::1']
     ];
 
     $config['bootstrap'][] = 'gii';
