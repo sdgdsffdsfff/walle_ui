fb8373334923437aa252bf51b987540694dfec3c
zhaolu <zhaolu@playcrab.com>
2016-02-22 11:40:37 +0800

    修改

--- views/layouts/left.php
+++ views/layouts/left.php
@@ -15,7 +15,7 @@ use yii\helpers\Html;
         </div>
         <ul class="nav" id="side-menu">
             <?php foreach($this->params['menuData'] as $menu){ ?>
-                <li <?php if($this->context->id == $menu['controller']){ ?>class="active"<?php } ?>>
+                <li <?php if(isset($menu['controller']) && ($this->context->id == $menu['controller'])){ ?>class="active"<?php } ?>>
                     <a href="<?= $menu['path']; ?>"><i class="fa <?= $menu['icon']; ?>"></i>&nbsp;&nbsp;&nbsp;<span class="nav-label"><?= $menu['name']; ?></span><?php if(count($menu['subMenu']) > 0){ ?><span class="fa arrow"></span><?php } ?></a>
                     <?php if(isset($menu['subMenu']) && count($menu['subMenu']) > 0){ ?>
                         <ul class="nav nav-second-level">
