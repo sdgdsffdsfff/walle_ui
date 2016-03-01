1a2b1b455fa1afd8fdd9af1b9e30f39eab9f1a33
zhaolu <zhaolu@playcrab.com>
2016-03-01 10:45:09 +0800

    页面修改

--- views/clientpackage/tolist.php
+++ views/clientpackage/tolist.php
@@ -8,7 +8,7 @@ use yii\widgets\LinkPager;
     <div class="hpanel">
         <div class="panel-body">
             <h5 class="font-light m-b-xs">
-                查看安装包
+                查看安装包列表
             </h5>
         </div>
     </div>
