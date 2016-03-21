c1a6eae730c25c5ba2235f66f41e8682a8b16c7f
zhaolu <zhaolu@playcrab.com>
2016-03-21 16:13:14 +0800

    修改文字描述

--- views/region/list.php
+++ views/region/list.php
@@ -8,7 +8,7 @@ use yii\widgets\LinkPager;
     <div class="hpanel">
         <div class="panel-body">
             <h5 class="font-light m-b-xs">
-                查看发行区域
+                查看发行地区
             </h5>
         </div>
     </div>
