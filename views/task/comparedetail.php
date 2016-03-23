f679d2d50435d3fa7435f49743399ce9e7351572
zhaolu <zhaolu@playcrab.com>
2016-03-23 10:05:14 +0800

    修改

--- views/task/comparedetail.php
+++ views/task/comparedetail.php
@@ -60,7 +60,7 @@ use yii\helpers\Html;
                             </tr>
                             <?php if($jobConfigs){ ?>
                             <tr>
-                                <th></th>
+                                <th>任务ID</th>
                                 <th></th>
                                 <?php for($i = 0;$i < count($jobConfigs); $i++){ ?>
                                 <th><?= $jobId_arr[$i] ?></th>
